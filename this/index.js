4d882fb6a5e63c72e58e15c7d770472d9c7b1981
Bodea Denis <denis_gabriel94@yahoo.com>
2022-05-11 23:44:37 +0300

    add new things

--- this/index.js
+++ this/index.js
@@ -1,24 +1,33 @@
 // This keyword
 
 const user = {
+  name: 'Denis',
+  sayHi() {}
 }
 
 const admin = {
+  name: 'Administrator',
 } 
 
 // Why is useful this keyword?
 
 const user2 = {
+  name: 'Denis',
 }
 const admin2 = {
+  name: 'Administrator',
 }
 
 // A small problem that u can encounter
 
 const person = {
+  name: 'John',
+  walk() {}
 }
 
 // The fix
 
 const fixedPerson = {
+  name: 'John',
+  walk() {}
 }
